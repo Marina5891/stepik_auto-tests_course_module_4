7875063879ef0b58f5ed5fc05d4374db00c97638
Marina5891 <marinachist1985@gmail.com>
2023-10-12 19:33:51 +0300

    Adds a check for the presence of a login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -11,3 +12,6 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     # проверяем наличие на странице ссылки на страницу логина
     page.should_be_login_link()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
